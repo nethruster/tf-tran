40d999d12a09f914efbaa4cab9859399a06b5bdc
gariasf <accounts@gariasf.com>
2018-12-30 02:32:38 +0100

    Implement route listing from server

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,40 +1,68 @@
-import { h } from 'preact'
+import { h, Component } from 'preact'
+import { connect } from 'unistore/preact'
+import { bind } from 'decko'
+import { Link } from 'react-router-dom'
 
 import Icon from 'icon'
 
+import { actions } from 'store'
+
 import style from './styles.scss'
 
-export default function Home () {
-  return (
-    <div class={style.homeWrapper}>
-      <div class={`flex flex-main-center ${style.lineListingContainer}`} data-line="1">
-        <ul>
-          <li class={`flex flex-cross-center flex-sb  ${style.listingItem}`}>
-            <p class={`truncate ${style.stopTitle}`}>Intercambiador</p>
+export default connect('routes', actions)(class Home extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isFetching: false
+    }
+  }
+
+  componentDidMount() {
+    this.props.updateRoutes()
+  }
+
+  @bind
+  renderRouteStops(route, stops) {
+    if(!stops) {
+      return <p>No se han podido cargar las paradas</p>
+    }
+
+    return Object.keys(stops).map(stop => {
+      return (
+        <Link to={`/route/${route}/stop/${stop}`} class={style.listingItemLink}>
+          <li class={`flex flex-cross-center flex-sb ${style.listingItem}`}>
+            <p class={`truncate ${style.stopTitle}`}>{stop}</p>
             <div class={`flex flex-full-center ${style.listingItemIcon}`}>
               <Icon name="next" color="var(--color-secondary)" size="28" />
             </div>
           </li>
-          <li class={`flex flex-cross-center flex-sb  ${style.listingItem}`}>
-            <p class={`truncate ${style.stopTitle}`}>Fundación</p>
-            <div class={`flex flex-full-center ${style.listingItemIcon}`}>
-              <Icon name="next" color="var(--color-secondary)" size="28" />
-            </div>
-          </li>
-          <li class={`flex flex-cross-center flex-sb  ${style.listingItem}`}>
-            <p class={`truncate ${style.stopTitle}`}>Teatro Guimerá</p>
-            <div class={`flex flex-full-center ${style.listingItemIcon}`}>
-              <Icon name="next" color="var(--color-secondary)" size="28" />
-            </div>
-          </li>
-          <li class={`flex flex-cross-center flex-sb  ${style.listingItem}`}>
-            <p class={`truncate ${style.stopTitle}`}>Weyler</p>
-            <div class={`flex flex-full-center ${style.listingItemIcon}`}>
-              <Icon name="next" color="var(--color-secondary)" size="28" />
-            </div>
-          </li>
-        </ul>
+        </Link>
+      )
+    })
+  }
+
+  @bind
+  renderLines() {
+    if(!this.props.routes) {
+      return <p>No se han podido cargar las paradas</p>
+    }
+
+    return this.props.routes.map(route => {
+      return ( 
+        <div class={`flex flex-main-center ${style.lineListingContainer}`} data-line={route.name}>
+          <ul>
+            {this.renderRouteStops(route.name, route.stops)}
+          </ul>
+        </div>
+      )})
+  }
+  
+  render({routes}) {
+    return (
+      <div class={style.homeWrapper}>
+       { routes === null ? <p>Cargando datos...</p> : this.renderLines() }
       </div>
-    </div>
-  )
-}
+    )
+  }
+})
