8b4b31f40b585bb37e2ce68a378ae36c4f4029e6
gariasf <accounts@gariasf.com>
2018-12-30 02:32:14 +0100

    Add store provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import { h, render, Component } from 'preact'
 import { bind } from 'decko'
+import { Provider } from 'unistore/preact'
 import 'preact/debug' // TODO Remove on prod
 
 import ContentShell from 'content-shell'
+
+import { store } from 'store'
 
 import './styles/index.scss'
 
 const mountPoint = document.getElementById('mount-point')
 
-render(<ContentShell />, mountPoint, mountPoint.lastElementChild)
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ContentShell />
+      </Provider>
+    )
+  }
+}
+
+render(<App />, mountPoint, mountPoint.lastElementChild)
 
