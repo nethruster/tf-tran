63e3de706f19d31e6c6154546234c0eb7b217e5f
gariasf <accounts@gariasf.com>
2019-01-01 21:59:26 +0100

    Improve remaining minutes string

--- src/scripts/tranvia-api/get-remaining-time-string.js
+++ src/scripts/tranvia-api/get-remaining-time-string.js
@@ -7,7 +7,29 @@ export default function (remainingMinutes) {
       return "1 minuto";
       break;
     default:
-      return `${remainingMinutes} minutos`;
+      return _minutesToHoursAndSeconds(remainingMinutes);
       break;
+  }
+}
+
+function _minutesToHoursAndSeconds(minutes) {
+  let hours = minutes / 60;
+  let roundHours = Math.floor(hours);
+  let remainderMinutes = (hours - roundHours) * 60;
+  let roundMinutes = Math.round(remainderMinutes);
+
+  let hoursString = `${roundHours} hora${roundHours > 1 ? 's' : ''}`
+  let minutesString = `${roundMinutes} minutos`
+
+  if(roundHours > 0 && roundMinutes > 0) {
+    return `${roundHours} y ${roundMinutes}`
+  } 
+
+  if(roundHours > 0) {
+    return hoursString
+  }
+
+  if(roundMinutes > 0) {
+    return minutesString
   }
 }
