e2f799e2d01685079f5df4f5f38ab94136653e57
gariasf <accounts@gariasf.com>
2019-01-19 17:09:51 +0100

    Change banner logo versioning

--- src/components/header/logo/index.js
+++ src/components/header/logo/index.js
@@ -11,7 +11,7 @@ export default function HeaderLogo() {
   return (
     <div class="come-in" style="animation-delay: .4s;">
       <h1 class={`flex flex-full-center ${style.logoTitle}`}>
-        <img src="/assets/img/logo.svg" alt="Próximo Tranvia"></img>
+        <img src="/assets/img/logo.svg?v=19012019" alt="Próximo Tranvia"></img>
       </h1>
     </div>
   );
