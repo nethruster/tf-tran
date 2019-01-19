8f2244aaae06968465b0732400ee55ad049bcf23
gariasf <accounts@gariasf.com>
2019-01-19 16:23:21 +0100

    Fix img assets generation

--- src/components/header/logo/index.js
+++ src/components/header/logo/index.js
@@ -4,11 +4,14 @@ import Icon from "icon";
 
 import style from "./styles.scss";
 
+// Notice me file-loader (●´ω｀●)
+import "../../../assets/img/logo.svg";
+
 export default function HeaderLogo() {
   return (
     <div class="come-in" style="animation-delay: .4s;">
       <h1 class={`flex flex-full-center ${style.logoTitle}`}>
-        <img src="/assets/logo.svg" alt="Próximo Tranvia"></img>
+        <img src="/assets/img/logo.svg" alt="Próximo Tranvia"></img>
       </h1>
     </div>
   );
