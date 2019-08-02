7752b7bcd34e16c559596f071b229e432ee317ff
Claudio Yanes <me@claudio4.com>
2019-08-02 21:22:59 +0100

    Fix stops sorting

--- src/scripts/tranvia-api/parse-data.js
+++ src/scripts/tranvia-api/parse-data.js
@@ -1,4 +1,5 @@
 import { capitalize } from "../utils";
+import stopsList from './stops-list';
 
 export default function parseData(data) {
   if (!data) {
@@ -26,9 +27,9 @@ export default function parseData(data) {
   });
 
   for (let i = routes.length - 1; i >= 0; i--) {
+    routes[i].stops = getSortedStops(stopsList[i], routes[i].stops)
     sortRouteStopArrivals(routes[i]);
   }
-
   return routes;
 }
 
@@ -41,4 +42,17 @@ function sortRouteStopArrivals(route) {
     route.stops[item][0].sort(sortStopArrivals);
     route.stops[item][1].sort(sortStopArrivals);
   });
+}
+
+function getSortedStops(stopList, stops) {
+  let sortedStops = {}
+  Object.keys(stops).sort((stopA, stopB) => {
+    let posA = stopList[stopA]
+    if (posA === undefined) { return 0 } 
+    let posB = stopList[stopB]
+    if (posB === undefined) { return 0 } 
+
+    return posA - posB
+  }).forEach(key => {sortedStops[key] = stops[key]})
+  return sortedStops
 }
---
+++ src/scripts/tranvia-api/stops-list.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    "Intercambiador": 0,
+    "Fundación": 1,
+    "Teatro Guimerá": 2,
+    "Weyler": 3,
+    "La Paz": 4,
+    "Puente Zurita": 5,
+    "Cruz Del Señor": 6,
+    "Conservatorio": 7,
+    "Chimisay": 8,
+    "Príncipes De España": 9,
+    "Hospital La Candelaria": 10,
+    "Taco": 11,
+    "El Cardonal": 12,
+    "Hospital Universitario": 13,
+    "Las Mantecas": 14,
+    "Campus Guajara": 15,
+    "Gracia": 16,
+    "Museo De La Ciencia": 17,
+    "Cruz De Piedra": 18,
+    "Padre Anchieta": 19,
+    "La Trinidad": 20
+  },
+  {
+    "Tincer": 0,
+    "San Jerónimo": 1,
+    "El Cardonal": 2,
+    "Hospital Universitario": 3,
+    "Ingenieros": 4,
+    "La Cuesta": 5
+  }
+]
