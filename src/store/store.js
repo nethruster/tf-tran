399d7c4d1644b46c3f9dee1f843b1affeed909b2
Claudio Yanes <me@claudio4.com>
2019-10-04 15:19:57 +0100

    Remove accidentally added devtools

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,9 @@ import createStore from "unistore";
 
 import initialStore from "./initial-store.js";
 import actions from "./actions";
-import devtools from 'unistore/devtools'
 
 let store = null;
 
-store = devtools(createStore(initialStore));
+store = createStore(initialStore);
 
 export { store, actions };
