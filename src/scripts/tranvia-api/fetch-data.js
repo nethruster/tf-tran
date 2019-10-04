5ff906a8e954727d12b02a8d6ee10d861effb3cd
Claudio Yanes <me@claudio4.com>
2019-10-04 13:56:57 +0100

    New error when HTTP 502 is received

--- src/scripts/tranvia-api/fetch-data.js
+++ src/scripts/tranvia-api/fetch-data.js
@@ -3,6 +3,10 @@ import { DEFAULT_API_URI } from "vars";
 export default function fetchData(uri = DEFAULT_API_URI) {
   return fetch(uri).then(response => {
     if (!response.ok) {
+      if (response.status === 502) {
+        throw "MetroTenerife failed";
+      }
+
       throw "Response was not OK";
     }
     return response.json();
